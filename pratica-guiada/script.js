fa8b13061e421eb6a285160be98f28c098ce6983
Rafael Machado <machadorafael27@gmail.com>
2023-05-08 20:49:59 -0300

    testes em js

--- pratica-guiada/script.js
+++ pratica-guiada/script.js
@@ -1,1 +1,25 @@
 // Acompanhe a Prática Guiada e repita o exercício no Momento de Réplica!
+
+/*const imprimirNome = function (nome) {
+    console.log(`Olá,${nome}`);
+}
+
+imprimirNome("Ana");
+imprimirNome("Pirulito");
+imprimirNome("Bartô");
+
+--------------------------------------
+const fazerOperacao = (numero) => {
+    const verifica = numero % 2 === 0;
+    const soma = numero + 10;
+    const multiplica = numero * numero;
+
+    return `O número ${numero} é par?${verifica}. Soma com 10 o resultado é ${soma}.Multiplicado por ele mesmo o resultado é ${multiplica}.`;
+}
+console.log(fazerOperacao(12));*/
+
+
+
+
+
+
